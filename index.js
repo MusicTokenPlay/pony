52f17dc1af3edf7133a9e29428c1c3af0395779c
MusicTokenPlay <santa_fe_coin@yahoo.com>
2024-07-28 05:58:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ const { Telegraf } = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => {
-  ctx.reply('Нажмите кнопку "Play", чтобы начать игру:', {
-    reply_markup: {
-      inline_keyboard: [
-        [{ text: 'Play', url: 'https://musictokenplay.github.io/pony/' }]
-      ]
-    }
-  });
+  ctx.replyWithGame(process.env.GAME_SHORT_NAME);
 });
 
 bot.launch().then(() => {
